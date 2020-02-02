9aac89470ee82aec37646f71e61772d6d272b90d
Steffen CUcos <steffen.cucos@gmail.com>
2020-02-02 10:55:04 -0500

    Move token out of github

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws LoginException {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
-        String token = "NDg4MDUzMDc3NDU1MTQyOTIy.DnWmhA.oJ5Pwf2WjUf7hq7kNZCWyrOjQzY";
+        String token = new Config().token;
         builder.setToken(token);
         builder.addEventListener(new DefaultEventListener());
         builder.build();
